2a90f71bfaf74cdb0e925bb1622fdb990d89fbdc
Seonu1015 <ulxovoxlu1015@gmail.com>
2024-04-09 22:44:42 +0900

    composable

---
+++ pages/profile.vue
@@ -0,0 +1,10 @@
+<script setup>
+const { sayHello } = useUtils();
+sayHello();
+</script>
+
+<template>
+  <div class="Main container py-3 mx-auto">
+    <h1 class="text-teal-500">Hello World</h1>
+  </div>
+</template>
